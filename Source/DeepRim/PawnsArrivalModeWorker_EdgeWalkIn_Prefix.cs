e5aea6a610af5df9dd05ed336ec609e85d7fa4c2
SunPraise <xenothestrange@yahoo.com>
2024-04-06 14:03:47 -0700

    Reworked EdgeWalkIn_Prefix
    Pawns will now spawn underground if there is any valid place for them to arrive at the edge of the map. If not, they will spawn above ground.

--- Source/DeepRim/PawnsArrivalModeWorker_EdgeWalkIn_Prefix.cs
+++ Source/DeepRim/PawnsArrivalModeWorker_EdgeWalkIn_Prefix.cs
@@ -12,17 +12,25 @@ public static class PawnsArrivalModeWorker_EdgeWalkIn_Patch
     {
         Map map = (Map)parms.target;
         var lift = map.listerBuildings.AllBuildingsColonistOfClass<Building_SpawnedLift>().FirstOrDefault();
+
         if (lift != null){
-            Log.Message($"Found lift: {lift}. Looks like you're trying to spawn pawns underground! Fixing...");
+            DeepRimMod.LogMessage($"Found a lift: {lift}. Checking for valid underground entry point...");
+
+            IEnumerable<IntVec3> validEdgeCells = CellFinder.mapEdgeCells.Where(cell => cell.Walkable(map));
+            if (!validEdgeCells.Any()){
+                DeepRimMod.LogMessage("Couldn't find a valid underground entrance on this layer, spawning aboveground instead.");
+                map = lift.parentDrill.Map;
+            }
+
+            IntVec3 cell = CellFinder.mapEdgeCells.Where(cell => cell.Walkable(map)).RandomElement();
             for (int i = 0; i < pawns.Count; i++)
             {
-                Map parentMap = lift.parentDrill.Map;
-                IntVec3 cell = CellFinder.RandomEdgeCell(parentMap);
-                IntVec3 loc = CellFinder.RandomClosewalkCellNear(cell, parentMap, 20);
-                GenSpawn.Spawn(pawns[i], loc, parentMap, parms.spawnRotation);
+                IntVec3 loc = CellFinder.RandomClosewalkCellNear(cell, map, 8);
+                GenSpawn.Spawn(pawns[i], loc, map, parms.spawnRotation);
             }
         return false;
         }
-        return true;
+
+    return true;
     }
 }
